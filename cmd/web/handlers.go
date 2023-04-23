ad27c3e8bf31a3d0020a9602d5e5e12153ed4a6b
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-23 16:34:12 +0300

    Организация файлов веб-приложения

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -34,15 +33,4 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d: ", id)
 	fmt.Fprintf(w, "%s", r.URL.Path)
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }

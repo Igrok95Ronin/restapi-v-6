3195d0a9018afd956a979e41ea7af3d76bc82bbd
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-23 14:17:23 +0300

    HTTP заголовки

--- main.go
+++ main.go
@@ -16,6 +16,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
 	w.Write([]byte("Отображение заметки..."))
 }
 

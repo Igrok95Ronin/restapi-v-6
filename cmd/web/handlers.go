675363a4074d1ea7c3dc78ca1bf609106cc027e3
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-27 16:03:29 +0300

    Добавлена страница Контакты

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -76,5 +76,20 @@ func about(w http.ResponseWriter, r *http.Request) {
 }
 
 func contacts(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("contacts"))
+	files := []string{
+		"./ui/html/contacts.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.partial.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
